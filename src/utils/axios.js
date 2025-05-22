2328c373adface712c0fadd065ed68c272a2bd8d
simonkotslay <164027164+simonkotslay@users.noreply.github.com>
2025-05-21 23:24:35 -0600

    commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,3 +5,4 @@ const instance = axios.create({
     baseURL: 'http://api.themoviedb.org/3'
 })
 export default instance;
+// http://api.themoviedb.org/3/discover/movie?api_key=98f71968ee882b86584029e4a671719f&with_genres=35
